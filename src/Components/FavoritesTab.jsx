0a8f6eea0ea7607fda62eebf3e60a49a78049cc7
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 15:43:02 +0200

    ADD FavoritesTab style

--- src/Components/FavoritesTab.jsx
+++ src/Components/FavoritesTab.jsx
@@ -5,18 +5,28 @@ function FavoritesTab() {
     useGlobalContext();
 
   return (
-    <div className={``}>
-      <div>
-        <button>Close</button>
+    <div
+      className={`z-1 fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr] transition duration-300 ease-in-out ${
+        showFavorites
+          ? "translate-x-0 opacity-100"
+          : "translate-x-full opacity-0"
+      }`}
+    >
+      <div className="grid gap-5 p-4">
+        <button className="cursor-pointer bg-black text-white">Close</button>
       </div>
       <div>
-        <h2></h2>
-        <div>
-          <ul>
+        <h2 className="p-5 text-white text-2xl">Favorites</h2>
+        <div className="p-5">
+          <ul className="flex flex-col gap-10 justify-between  text-white p-2 border-b-2 border-slate-700 round">
             {favorites.map((item, i) => (
-              <li key={i}>{item.title}</li>
+              <li className="bg-slate-500 p-3" key={i}>
+                {item.title}
+              </li>
             ))}
-            <button>Remove from favorites</button>
+            <button className="cursor-pointer py-1 px-3  mx-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10  dark:focus:ring-gray-700 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-800">
+              Remove from favorites
+            </button>
           </ul>
         </div>
       </div>

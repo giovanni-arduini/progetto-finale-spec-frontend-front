3ee3f79191039f36635339c5a7e8538f7deea1d6
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 13:00:26 +0200

    ADD Header component with favorites toggle functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,25 @@
+import { NavLink } from "react-router-dom";
+import { useGlobalContext } from "../Contexts/GlobalContext";
+import FavoritesTab from "./FavoritesTab";
+
 export default function Header() {
-  return;
+  const { showFavorites, setShowFavorites } = useGlobalContext();
+
+  const handleShowFavorites = () => {
+    !showFavorites ? setShowFavorites(true) : setShowFavorites(false);
+  };
+
+  return (
+    <header>
+      <FavoritesTab />
+      <div>
+        <NavLink to={"/"}>Home</NavLink>
+        <button>Compare products</button>
+      </div>
+
+      <div>
+        <div onClick={() => handleShowFavorites()}>Favorites</div>
+      </div>
+    </header>
+  );
 }

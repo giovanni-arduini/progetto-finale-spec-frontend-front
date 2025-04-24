02285c4ccdf584b0a3cae0ab7e603013d7966385
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 10:43:44 +0200

    ADD DefaultLayout component with Header and Footer

---
+++ src/Layouts/DefaultLayout.jsx
@@ -0,0 +1,12 @@
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+
+export default function DefautlLayout() {
+  return (
+    <div>
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+}

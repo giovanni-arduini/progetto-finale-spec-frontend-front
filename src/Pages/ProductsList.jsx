d71ac7998d51c2bf1e8972b18c4a64279b233839
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 11:53:19 +0200

    ADD initial structure for ProductsList component

--- src/Pages/ProductsList.jsx
+++ src/Pages/ProductsList.jsx
@@ -1,3 +1,18 @@
 export default function ProductsList() {
-  return;
+  return (
+    <>
+      <section>
+        <h1>Guitars</h1>
+
+        <div>
+          <input type="text" />
+
+          <select name="" id=""></select>
+        </div>
+
+        <button>Sort by name</button>
+        <div></div>
+      </section>
+    </>
+  );
 }

4826089a7cc9672bf0c93382ff6d52a153ee6e28
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 10:49:13 +0200

    ADD wrap BrowserRouter into GlobalProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalProvider } from "./Contexts/GlobalContext";
 import DefautlLayout from "./Layouts/DefaultLayout";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefautlLayout />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefautlLayout />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
     </>
   );
 }

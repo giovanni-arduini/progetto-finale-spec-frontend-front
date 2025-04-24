ffba7635f2ec4ad902eac782d18d436274fd6a34
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 12:31:27 +0200

    ADD ProductCard component for displaying guitar details

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { useGlobalContext } from "../Contexts/GlobalContext";
+
+function ProductCard({ product }) {
+  const { id, title, category } = product;
+
+  const navigate = useNavigate();
+
+  const toDetail = (id) => {
+    navigate(`/guitars/${id}`);
+  };
+
+  return (
+    <div>
+      <div onClick={() => toDetail(id)}>
+        <h2>{title}</h2>
+        <p>Price</p>
+        <p>{category}</p>
+      </div>
+      <div>
+        <button>♥︎</button>
+        <button>⇄</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;

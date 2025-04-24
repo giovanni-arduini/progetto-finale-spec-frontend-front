e8d3fe0dcbeadf2bd38c003c2681448a022137b5
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 12:44:22 +0200

    ADD favorites management to GlobalContext for handling user favorites

--- src/Contexts/GlobalContext.jsx
+++ src/Contexts/GlobalContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import useGuitars from "../Hooks/useGuitars";
 
 const GlobalContext = createContext();
@@ -10,8 +10,21 @@ export function useGlobalContext() {
 export function GlobalProvider({ children }) {
   const { products, setProducts, getProduct } = useGuitars();
 
+  const [favorites, setFavorites] = useState([]);
+  const [showFavorites, setShowFavorites] = useState(false);
+
   return (
-    <GlobalContext.Provider value={{ products, setProducts, getProduct }}>
+    <GlobalContext.Provider
+      value={{
+        products,
+        setProducts,
+        getProduct,
+        favorites,
+        setFavorites,
+        showFavorites,
+        setShowFavorites,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );

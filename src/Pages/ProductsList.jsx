3d8c0c8ca62caf62a5ee1ae0093cd6fc15c25078
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 11:55:25 +0200

    ADD search input and category select to ProductsList component

--- src/Pages/ProductsList.jsx
+++ src/Pages/ProductsList.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+import { useGlobalContext } from "../Contexts/GlobalContext";
+
 export default function ProductsList() {
+  const { products } = useGlobalContext;
+  const [search, setSearch] = useState("");
+
   return (
     <>
       <section>
         <h1>Guitars</h1>
 
         <div>
-          <input type="text" />
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
 
-          <select name="" id=""></select>
+          <select
+            name="category"
+            id="category"
+            onChange={(e) => setSelectCategory(e.target.value)}
+          ></select>
         </div>
 
         <button>Sort by name</button>

e6bc6292690945bec060e21242350fd53ab83a61
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 10:43:33 +0200

    ADD routing setup with BrowserRouter and DefaultLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefautlLayout from "./Layouts/DefaultLayout";
 
 function App() {
-  return;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefautlLayout />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;

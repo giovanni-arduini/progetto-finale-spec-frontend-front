eb4f9154f37e805a215c122b91ed559da7ccf37a
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-24 10:51:41 +0200

    ADD ProductsDetail and ProductsList components with basic structure; update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalProvider } from "./Contexts/GlobalContext";
 import DefautlLayout from "./Layouts/DefaultLayout";
+import ProductsDetail from "./Pages/ProductDetail";
+import ProductsList from "./Pages/ProductsList";
 
 function App() {
   return (
@@ -9,7 +11,10 @@ function App() {
       <GlobalProvider>
         <BrowserRouter>
           <Routes>
-            <Route element={<DefautlLayout />}></Route>
+            <Route element={<DefautlLayout />}>
+              <Route path="/" element={<ProductsList />}></Route>
+              <Route path="/guitars/:id" element={<ProductsDetail />}></Route>
+            </Route>
           </Routes>
         </BrowserRouter>
       </GlobalProvider>

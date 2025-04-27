f42cab8c1406bc3ef12fd42ae4fa746dde8330e1
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-27 11:50:44 +0200

    ADD CompareProvider to App component for improved state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalProvider } from "./Contexts/GlobalContext";
+import { CompareProvider } from "./Contexts/CompareContext";
 import DefaultLayout from "./Layouts/DefaultLayout";
 import ProductsDetail from "./Pages/ProductDetail";
 import ProductsList from "./Pages/ProductsList";
@@ -9,14 +10,16 @@ function App() {
   return (
     <>
       <GlobalProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<DefaultLayout />}>
-              <Route path="/" element={<ProductsList />}></Route>
-              <Route path="/guitars/:id" element={<ProductsDetail />}></Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <CompareProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<DefaultLayout />}>
+                <Route path="/" element={<ProductsList />}></Route>
+                <Route path="/guitars/:id" element={<ProductsDetail />}></Route>
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </CompareProvider>
       </GlobalProvider>
     </>
   );

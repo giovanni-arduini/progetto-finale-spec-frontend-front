378175421cd717a73512e40242c5b7f8b8abec04
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-27 13:45:57 +0200

    ADD position prop to SideTab

--- src/Components/SideTab.jsx
+++ src/Components/SideTab.jsx
@@ -1,4 +1,11 @@
-export default function SideTab({ title, content, onClose, show, onRemove }) {
+export default function SideTab({
+  title,
+  content,
+  onClose,
+  show,
+  position,
+  onRemove,
+}) {
   return (
     <div
       className={`z-1 fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr] transition duration-300 ease-in-out ${

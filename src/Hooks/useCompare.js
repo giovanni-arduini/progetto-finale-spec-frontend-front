757e855ce90a8e4c58c0103b0c12434dc2e1bb24
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-27 11:46:17 +0200

    FIX typo in useCompare hook for setShowCompare function

--- src/Hooks/useCompare.js
+++ src/Hooks/useCompare.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
 function useCompare() {
-  const [showCompare, setShowComapre] = useState(false);
+  const [showCompare, setShowCompare] = useState(false);
   const [itemsToCompare, setItemsToCompare] = useState([]);
 
-  return { showCompare, setShowComapre, itemsToCompare, setItemsToCompare };
+  return { showCompare, setShowCompare, itemsToCompare, setItemsToCompare };
 }
 
 export default useCompare;

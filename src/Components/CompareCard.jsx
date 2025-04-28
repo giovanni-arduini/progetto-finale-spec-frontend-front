c651fc3c47fca17514d11ab65a1f6c3d568a13cb
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-28 10:20:50 +0200

    REFACTOR update CompareCard layout and structure for better readability

--- src/Components/CompareCard.jsx
+++ src/Components/CompareCard.jsx
@@ -33,7 +33,7 @@ export default function CompareCard({ item }) {
 
   return (
     <>
-      <div className="bg-white p-10">
+      <div className="bg-slate-400 p-10">
         <div className="flex flex-col">
           <h3 className="text-3xl font-bold">{title}</h3>
           <div className="flex justify-between bg-white">
@@ -46,11 +46,22 @@ export default function CompareCard({ item }) {
               {<p className={availability.style}>{availability.message}</p>}
             </div>
           </div>
-          {itemSpecs.map(([key, value], index) => (
-            <p key={index}>
-              <strong>{sanitizeSpec(key)}:</strong> {value}
-            </p>
-          ))}
+          <table className="table-auto border-separate w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-200">
+            <thead></thead>
+            <tbody>
+              {itemSpecs.map(([key, value], index) => (
+                <tr
+                  className="odd:bg-white odd:dark:bg-gray-800 even:bg-gray-50 even:dark:bg-gray-700 border-b dark:border-gray-600 border-gray-200"
+                  key={index}
+                >
+                  <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {sanitizeSpec(key)}:
+                  </th>
+                  <td className="px-6 py-4">{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>

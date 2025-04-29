8aea3a1a0b586b622220481cb4e4ebf2979d43a8
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-29 12:37:35 +0200

    ADD FavTab style and overflow

--- src/Components/FavoritesTab.jsx
+++ src/Components/FavoritesTab.jsx
@@ -11,8 +11,11 @@ function FavoritesTab() {
       content={
         favorites.length > 0 ? (
           favorites.map((item, i) => (
-            <div className="bg-slate-500 p-3" key={i}>
-              {item.title}
+            <div
+              className="bg-slate-500 p-3 flex flex-col gap-2 overflow-auto"
+              key={i}
+            >
+              <div className="text-center">{item.title}</div>
               <button
                 className="cursor-pointer py-1 px-3  mx-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10  dark:focus:ring-gray-700 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-800"
                 onClick={() => removeFromFavorites(item)}
--- src/Components/SideTab.jsx
+++ src/Components/SideTab.jsx
@@ -3,7 +3,7 @@ export default function SideTab({ title, content, onClose, show, position }) {
     <div
       className={`${
         position !== "bottom"
-          ? `z-1 fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr] transition duration-300 ease-in-out"
+          ? `overflow-auto z-1 fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr] transition duration-300 ease-in-out"
           ${show ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`
           : ` overflow-auto fixed bottom-0 left-0 right-0 bg-gray-700 shadow-2xl h-100 w-full transition-transform transition-opacity duration-300 ease-in-out"
       ${

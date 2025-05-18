e020bd19d41f62be5251698ee421d4c616316610
giovanni-arduini <gionnibigudd@gmail.com>
2025-05-18 16:41:40 +0200

    ADD database and type definition

---
+++ types.ts
@@ -0,0 +1,28 @@
+export type Guitar = {
+  readonly id: number;
+  title: string;
+  price: number;
+  image: string;
+  category: string;
+  available: boolean;
+  readonly createdAt: number;
+  updatedAt?: number;
+  model?: string;
+  top?: string;
+  body?: string;
+  neck?: string;
+  fretboard?: string;
+  inlays?: string;
+  binding?: string;
+  profile?: string;
+  nutWidth?: number;
+  scale?: number;
+  bridge?: string;
+  pickUp?: string;
+  controls?: string;
+  electronic?: string;
+  color: string;
+  case?: string;
+  madeIn: string;
+  rating?: number;
+};

92353b89acedb0d044d9fd88225cb36e5e1d5ae8
giovanni-arduini <gionnibigudd@gmail.com>
2025-05-19 17:39:53 +0200

    ADD style to Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,16 +12,19 @@ export default function Header() {
   };
 
   return (
-    <header className="flex justify-between items-center px-3 bg-teal-600 p-3">
+    <header className="flex justify-between items-center px-10 p-3 ">
       <FavoritesTab />
       <div>
-        <NavLink to={"/"} className={`nav text-l font-semibold mr-5`}>
+        <NavLink
+          to={"/"}
+          className={`nav hover:text-indigo-100 text-l font-semibold mr-5 `}
+        >
           Home
         </NavLink>
         <button
-          id="compareBtn"
+          id="compareBtn "
           onClick={handleCompareClick}
-          className={`nav ${showCompare ? "active" : ""}`}
+          className={`hover:text-indigo-100 nav ${showCompare ? "active" : ""}`}
         >
           Compare products
         </button>
@@ -29,11 +32,18 @@ export default function Header() {
 
       <div>
         <div
-          className="w-10 h-10 bg-gray-200 rounded-full flex justify-center items-center relative"
+          className="w-10 h-10 bg-gray-200 rounded-full flex justify-center items-center relative hover:cursor-pointer"
+          id="favIcon"
           onClick={() => toggleFavorites()}
         >
           <img className="w-7" src="/images/save.png" alt="Favorites list" />
-          <span className="absolute p-2 top-1/2 left-1/2 bg-slate-500 text-white text-sm w-3 h-3 rounded-full flex justify-center items-center">
+          <div className="check text-yellow-500 absolute top-2 left-8 z-10 invisible">
+            ★
+          </div>
+          <span
+            id="favCounter"
+            className="absolute p-2 top-1/2 left-1/2 bg-slate-500 text-white text-sm w-3 h-3 rounded-full flex justify-center items-center"
+          >
             {favorites.length}
           </span>
         </div>

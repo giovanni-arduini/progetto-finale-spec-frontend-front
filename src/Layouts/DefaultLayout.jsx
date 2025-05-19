6b2200abc599fe5dd9ab96f1964eeb4362b2ead3
giovanni-arduini <gionnibigudd@gmail.com>
2025-05-19 17:41:51 +0200

    ADD showCompare clicking main element

--- src/Layouts/DefaultLayout.jsx
+++ src/Layouts/DefaultLayout.jsx
@@ -2,13 +2,22 @@ import { Outlet } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import CompareTab from "../Components/CompareTab";
+import { useCompareContext } from "../Contexts/CompareContext";
 
 export default function DefaultLayout() {
+  const { showCompare, setShowCompare } = useCompareContext();
+
+  const handleClickOnMain = () => {
+    showCompare ? setShowCompare(false) : "";
+  };
+
   return (
-    <div className="bg-zinc-200">
+    <div className="">
       <Header />
       <main className="w-[1200] max-w-full m-auto p-5">
-        <Outlet />
+        <div onClick={handleClickOnMain}>
+          <Outlet />
+        </div>
         <CompareTab />
       </main>
       <Footer />
